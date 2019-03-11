10acd70f5ce55c1b9229648098b35a7c959c400e
NickyEXE <dovern42@gmail.com>
2019-03-11 00:08:04 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Nicky Dover!"
+  end
+  get '/hometown' do
+    "My hometown is New York City, baby."
+  end
+  get '/favorite-song' do
+    "My favorite song is One Vision by Queen"
+  end
 end
